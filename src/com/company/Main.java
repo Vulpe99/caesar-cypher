c3b3cc1141c45392105de21b868abaab48c33100
Vulpe99 <marius6radoi@gmail.com>
2019-01-27 15:20:15 +0200

    -Encapsulated the encode procedure in a method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,36 +1,29 @@
 package com.company;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
 
-    public static void main(String[] args) {
-        int shift = 10;
-
-        String textToEncode = "Ana are zahar si Ion are mere!!";
-
+    private static StringBuilder encode(String text, int shift){
         StringBuilder sb = new StringBuilder();
-
-
-        for (int i = 0; i < textToEncode.length(); i++) {
+        for (int i = 0; i < text.length(); i++) {
             //cycle trough all the chars in our text
-            char ch = (char) textToEncode.toLowerCase().charAt(i);
+            char ch = text.toLowerCase().charAt(i);
             //check if it is a letter
-                if (Character.isLetter(ch)) {
-                        if ((char) (ch + shift) > 122) {
-                            char c = (char) (ch -(26-shift));
-                            sb.append(c);
-                        } else {
-                            char c = (char)(ch + shift);
-                            sb.append(c);
-                        }
+            if (Character.isLetter(ch)) {
+                if ((char) (ch + shift) > 122) {
+                    char c = (char) (ch - (26 - shift));
+                    sb.append(c);
+                } else {
+                    char c = (char) (ch + shift);
+                    sb.append(c);
+                }
 
-                } else sb.append(ch);
+            } else sb.append(ch);
         }
+        return sb;
+    }
 
-        //Print our new text
-        System.out.print(sb);
-
+    public static void main(String[] args) {
+        String textToEncode = "Ana are zahar si Ion are mere!!";
+        System.out.println(encode(textToEncode,10));
     }
 }
